9ce6a07621fadb7de35d53354d7cdb9ae5e3cdd8
wreulicke <wreulicke@gmail.com>
2022-09-16 17:34:41 +0900

    add handling for env

--- test.sh
+++ test.sh
@@ -6,13 +6,24 @@ while getopts e:t: OPTION
 do
     case $OPTION in
         t) TARGET=".$OPTARG";;
-        e) ENV=$OPTARG;;
+        e) ENV=".$OPTARG";;
     esac
 done
 
 ENVS=$(cat <<JSON
 {
     "infra": {
+      "development": {
+        "env": "development"
+      },
+      "staging": {
+        "env": "staging"
+      },
+      "production": {
+        "env": "production"
+      }
+    },
+    "app": {
       "development": {
         "env": "development"
       },
@@ -31,5 +42,9 @@ if [ -z "$TARGET" ]; then
   TARGET="[]" ## all
 fi
 
-matrix=$(echo $ENVS | jq -cr "{ include: [.$TARGET[]] }")
+if [ -z "$ENV" ]; then
+  ENV="[]" ## all
+fi
+
+matrix=$(echo $ENVS | jq -cr "{ include: [.$TARGET$ENV] }")
 echo ::set-output name=matrix::$matrix
